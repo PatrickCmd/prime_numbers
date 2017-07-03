f78018ba94de28d5f89adb0c57cb2ff9641f0781
Patrick Python Book <pwalukagga@gmail.com>
2017-07-03 16:47:06 +0300

    check if number is prime

--- prime_tests.py
+++ prime_tests.py
@@ -12,7 +12,12 @@ class primeTests(unittest.TestCase):
     def prime_number_less_than_one(self):
         number = self.prime.primeNumbers(-1)
         self.assertEqual(-1, number, 'Numbers less than 1 are not prime numbers')
-    
+
+    def prime_is_number_prime_or_not(self):
+        number = self.prime.primeNumbers(6)
+        result = number % 2
+        self.assertEqual(0, result, 'Number is not prime')
+
 
 if __name__ == '__main__':
     unittest.main()
