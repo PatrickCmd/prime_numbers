ee4b83c4be347f021cdca083c5efe4a56e7f5dfe
Patrick Python Book <pwalukagga@gmail.com>
2017-07-03 16:26:56 +0300

    check if value is number and not less than one

--- prime_tests.py
+++ prime_tests.py
@@ -6,4 +6,13 @@ class primeTests(unittest.TestCase):
     def setUp(self):
         self.prime = Primes()
 
+    def prime_returns_error_if_not_number(self):
+        self.assertRaises(ValueError, self.prime.primeNumbers, 'Value is not number')
     
+    def prime_number_less_than_one(self):
+        number = self.prime.primeNumbers(-1)
+        self.assertEqual(-1, number, 'Numbers less than 1 are not prime numbers')
+    
+
+if __name__ == '__main__':
+    unittest.main()
