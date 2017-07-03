637dab1a93135afd33bd9b4552782d8a38a181e5
Patrick Python Book <pwalukagga@gmail.com>
2017-07-03 17:14:07 +0300

    tracking start and end time

--- primes.py
+++ primes.py
@@ -1,14 +1,18 @@
+import time
+
 class Primes(object):
 
     def primeNumbers(self, n):
         '''Display prime numbers from 0 to n'''
+
+        start_time = time.time()
 
         # value must be a number and an integer
         if not isinstance(n, int):
             raise ValueError('Value must be an integer number. Try again!')
 
         print('Prime numbers from 0 to', n, 'are')
-        counter = 0
+        # counter = 0
         for num in range(0, n + 1):
             # prime numbers are greater than 1
             if num > 1:
@@ -17,6 +21,9 @@ class Primes(object):
                         break
                 else:
                     print(num, end=', ')
-                    counter += 1
-            else:
-                return "Value is negative. Please provide positive number"
+                    # counter += 1
+        end_time = time.time()
+
+        return start_time, end_time
+
+
